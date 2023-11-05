d2818f738e22128535f4efff884bf8d43e3c0c8c
Abdunazarov Nodirbek <devnodir@gmail.com>
2023-11-05 16:42:50 +0500

    updated

--- src/components/shared/Loaders/ScreenLoader.tsx
+++ src/components/shared/Loaders/ScreenLoader.tsx
@@ -1,6 +1,5 @@
 import { Spin } from 'antd'
 import React from 'react'
-import logo from "@/assets/logo.svg"
 import Style from './Style'
 
 const ScreenLoader: React.FC = () => {
@@ -8,7 +7,7 @@ const ScreenLoader: React.FC = () => {
 		<Style>
 			<div className='screen-loader'>
 				<div className="screen-loader-content">
-					<img src={logo} alt="" />
+					<img src="/logo.svg" alt="" />
 					<Spin />
 				</div>
 			</div>
