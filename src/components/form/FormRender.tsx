d735a687454033252f8ee92573165d9681d90308
Abdunazarov Nodirbek <devnodir@gmail.com>
2023-11-28 00:48:49 +0500

    unuseds deleted

--- src/components/form/FormRender.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FormRender: React.FC = () => {
-	return (
-		<div>FormRender</div>
-	)
-}
-
-export default FormRender
--- src/types/general.type.ts
+++ src/types/general.type.ts
@@ -1,14 +1,6 @@
-import { Control, FieldValues, Path, RegisterOptions } from "react-hook-form";
 import { IChildren } from "./helper.type";
 
 export interface ILayout {
     title?: string;
     children: IChildren;
-}
-
-export interface IFormInput<T extends FieldValues> {
-    control: Control<T>;
-    rules?: RegisterOptions<T>;
-    label?: string;
-    name: Path<T>;
 }
